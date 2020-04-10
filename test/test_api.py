ce0a1ac865411837e77e26184930a2950743c1e1
Jacobo de Vera <jacobo.devera@hotjar.com>
2020-04-10 17:30:39 +0100

    Add tests for the library API

---
+++ test/test_api.py
@@ -0,0 +1,13 @@
+import pylabeador
+
+
+def test_syllabify():
+    res = pylabeador.syllabify("tenacidad")
+    assert res == ['te', 'na', 'ci', 'dad']
+
+
+def test_syllabify_with_details():
+    res = pylabeador.syllabify_with_details("tenacidad")
+    assert res.hyphenated == "te-na-ci-dad"
+    assert res.stressed == 4
+    assert not res.accented
